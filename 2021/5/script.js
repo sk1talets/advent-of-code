c32830aa7a9ed8268da5bdad8cec79e1b279dfdd
Valeriy Kassenbayev <vkassenbaev@gmail.com>
2021-12-05 11:02:54 +0200

    Improve the day 5 solution

--- 2021/5/script.js
+++ 2021/5/script.js
@@ -16,7 +16,7 @@ function main() {
 
 	let overlaps = 0;
 
-	for (let [x1, y1, x2, y2] of vents) {
+	for (const [x1, y1, x2, y2] of vents) {
 		// part 1
 		// if (x1 != x2 && y1 != y2) {
 		// 	continue;
