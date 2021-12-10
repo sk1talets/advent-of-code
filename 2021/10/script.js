92fd584bf5cb00871f5aea06cfe5c7bcd04b6ab6
Valeriy Kassenbayev <vkassenbaev@gmail.com>
2021-12-10 10:05:30 +0200

    Add the solution for day 10

---
+++ 2021/10/script.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+
+function main() {
+	const chunks = fs.readFileSync('./chunks.txt')
+		.toString()
+		.split('\n')
+		.map(line => line.split(''));
+
+	const syntaxPoints = {
+		')': 3,
+		']': 57,
+		'}': 1197,
+		'>': 25137,
+	};
+
+	const completionPoints = {
+		')': 1,
+		']': 2,
+		'}': 3,
+		'>': 4,
+	};
+
+	const closing = {
+		'[': ']',
+		'{': '}',
+		'(': ')',
+		'<': '>',
+	};
+
+	const syntaxErrCounts = {};
+	const completionScores = [];
+
+	CHUNKS: for (const chunk of chunks) {
+		let completionScore = 0;
+		let opened = [];
+
+		for (const token of chunk) {
+			if (['(', '[', '{', '<'].includes(token)) {
+				opened.push(token);
+			} else if (closing[opened.pop()] !== token) {
+				syntaxErrCounts[token] = syntaxErrCounts[token] + 1 || 1;
+				continue CHUNKS;
+			}
+		}
+
+		while (opened.length) {
+			completionScore = completionScore * 5 + completionPoints[closing[opened.pop()]];
+		}
+
+		completionScores.push(completionScore);
+	}
+
+	let syntaxErrScore = 0;
+
+	for (const [token, n] of Object.entries(syntaxErrCounts)) {
+		syntaxErrScore += syntaxPoints[token] * n;
+	}
+
+	const middleCompletionScore = completionScores
+		.sort((a, b) => a - b)[Math.floor(completionScores.length/2)];
+
+	console.log('syntax errors score:', syntaxErrScore);
+	console.log('middle completion score:', middleCompletionScore);
+}
+
+main();
+
