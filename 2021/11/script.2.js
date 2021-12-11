8138e0e62e9034113e8a119c3076658fa879bf36
Valeriy Kassenbayev <vkassenbaev@gmail.com>
2021-12-11 11:36:32 +0200

    Add the solution for day 11

---
+++ 2021/11/script.2.js
@@ -0,0 +1,69 @@
+const fs = require('fs');
+
+function getAdjacent(map, x, y) {
+	return [
+		[x,     y - 1],
+		[x,     y + 1],
+		[x - 1,     y],
+		[x - 1, y - 1],
+		[x - 1, y + 1],
+		[x + 1, y - 1],
+		[x + 1,     y],
+		[x + 1, y + 1],
+	].filter(([x, y]) => y in map && x in map[y]);
+}
+
+function checkFlashes(map, x, y) {
+	let count = 0;
+
+	if (map[y][x] !== 10) {
+		return 0;
+	}
+
+	count++;
+
+	for (const [xx, yy] of getAdjacent(map, x, y)) {
+		if (map[yy][xx] > 9) {
+			continue;
+		}
+
+		map[yy][xx]++;
+
+		count += checkFlashes(map, xx, yy);
+	}
+
+	map[y][x] = 11; // handled
+
+	return count;
+}
+
+function traverse(map, callback) {
+	for (let y = 0; y < map.length; y++) {
+		for (let x = 0; x < map[y].length; x++) {
+			callback(x, y);
+		}
+	}
+}
+
+function main() {
+	const map = fs.readFileSync('./map.txt')
+		.toString()
+		.split('\n')
+		.map(line => line.split('').map(n => parseInt(n, 10)));
+
+	for (let step = 1;; step++) {
+		let count = 0;
+
+		traverse(map, (x, y) => map[y][x]++);
+		traverse(map, (x, y) => { count += checkFlashes(map, x, y); });
+		traverse(map, (x, y) => { map[y][x] > 9 ? map[y][x] = 0 : null; });
+
+		if (count === map.length * map[0].length) {
+			console.log('all flushed on step:', step);
+			break;
+		}
+	}
+}
+
+main();
+
